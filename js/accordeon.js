300ed795123d06a55ee9bd7fadce144404e90a2f
Dovzhenko-olga <73553889+Dovzhenko-olga@users.noreply.github.com>
2021-05-18 21:24:31 +0300

    fix hidden

--- js/accordeon.js
+++ js/accordeon.js
@@ -3,6 +3,7 @@ const featureSubElems = document.querySelectorAll('.feature-sub');
 
 featureLinkElems.forEach((btn, index) => {
   btn.addEventListener('click', () => {
+  if (!btn.classList.contains('feature__link_active')) {
     featureSubElems.forEach((featureSubElem) => {
       featureSubElem.classList.add('hidden');
     });
@@ -11,5 +12,9 @@ featureLinkElems.forEach((btn, index) => {
     });
     featureSubElems[index].classList.remove('hidden');
     btn.classList.add('feature__link_active');
+  } else {
+    btn.classList.remove('feature__link_active');
+    featureSubElems[index].classList.add('hidden');
+  }
   })
 });
